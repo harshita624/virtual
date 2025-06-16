24ae2456925b4ea330e1527e911d072b309286f1
harshita624 <142253286+harshita624@users.noreply.github.com>
2025-06-16 15:23:30 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,11 @@ import cors from "cors"
 import userRouter from "./routes/user.routes.js"
 import geminiResponse from "./gemini.js"
 const app=express()
-app.use(cors({
-    origin:"https://virtualfrontend.onrender.com",
-    credentials:true
+app.use(cors({ 
+    origin: ["http://localhost:5173", "https://virtualfrontend.onrender.com"],
+    credentials: true
 }))
+
 const port=process.env.PORT||5000
 app.use(express.json())
 app.use(cookieParser())
