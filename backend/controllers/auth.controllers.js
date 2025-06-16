15cb2905d8a6b19825fb26f115ab0519f8fe9191
harshita624 <142253286+harshita624@users.noreply.github.com>
2025-06-16 15:19:27 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -23,8 +23,8 @@ try{
   res.cookie("token",token,{
     httpOnly:true,
     maxAge:7*24*60*60*1000,
-    sameSite:"strict",
-    secure:false
+    sameSite:"None",
+    secure:true
   })
 
   return res.status(201).json(user)
@@ -53,8 +53,8 @@ try{
   res.cookie("token",token,{
     httpOnly:true,
     maxAge:7*24*60*60*1000,
-    sameSite:"strict",
-    secure:false
+     sameSite:"None",
+    secure:true
   })
 
   return res.status(200).json(user)
